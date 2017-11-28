8196766b43dbba521e10ec0e4c7bf6b1e1021433
tarngds <zoux14@mails.tsinghua.edu.cn>
2017-11-28 18:04:24 -0500

    modified:   time_test.py

--- time_test.py
+++ time_test.py
@@ -30,6 +30,25 @@ import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
 from networks.resblock import GoNetWork
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
 def main():
     input=torch.zeros(args.batch,19,19,17)
     input2=torch.zeros(args.batch,19,19,17)
