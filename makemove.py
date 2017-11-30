37434fb655f528cc0183a9fa980d439e840776ac
tarngds <zoux14@mails.tsinghua.edu.cn>
2017-11-29 22:44:54 -0500

    modified:   makemove.py 	modified:   tromp-taylor.py

--- makemove.py
+++ makemove.py
@@ -16,10 +16,12 @@ def makemove(state,move,type=0):
     mvx=move/19
     mvy=move%19
     if (move!=361):
+        if newstate[0][mvx][mvy]==1:
+            return newstate,1 #error!illegal move
         newstate[0][mvx][mvy]=1
 
     issearched=np.zeros(19,19)
-    def checkstate(i,j,w):
+    def checkstate(i,j,w,color):
         if issearched[i][j]>0:
             return -1
         issearched[i][j]=w
@@ -28,9 +30,9 @@ def makemove(state,move,type=0):
                 newx=i+i1
                 newy=i+i2
                 if (newx>=0) and (newx<=18) and (newy>=0) and (newy<=18):
-                    if newstate[0][newx][newy]+newstate[1][newx][newy]==0:
+                    if newstate[color][newx][newy]+newstate[1-color][newx][newy]==0:
                         return 0
-                    if newstate[1][newx][newy]==1:
+                    if newstate[1-color][newx][newy]==1:
                         k=checkstate(newx,newy)
                         if k==0:
                             return 0
@@ -44,12 +46,14 @@ def makemove(state,move,type=0):
                 newy=mvy+i2
                 if (newx>=0) and (newx<=18) and (newy>=0) and (newy<=18) and (newstate[1][newx][newy]==1):
                     w=w+1
-                    state=checkstate(newx,newy,w)
-                    if state==1:
+                    stat=checkstate(newx,newy,w,0)
+                    if stat==1:
                         for i in range (19):
                             for j in range(19):
                                 if issearched[i][j]==w:
                                     newstate[1][newx][newy]=0
+        if checkstate(mvx,mvy,w+1,1)==1:
+            return state,1 #error!illegal move
 
 #compare against previous stages to make sure there's no repetition
     err=0
--- tromp-taylor.py
+++ tromp-taylor.py
@@ -1,7 +1,69 @@
 #tromp--tayler scoring
+import torch
 
 def scoring(state,komi):
     
+    issearched=np.zeros(19,19)
+    def checkstate(i,j,w):
+        if issearched[i][j]>0:
+            return -1
+        issearched[i][j]=w
+        current=0
+        #0 , no #1, only 0, #2, only 1, #3, both
+        for i1 in [-1,1]:
+            for i2 in [-1,1]:
+                newx=i+i1
+                newy=i+i2
+                if current==3:
+                    return 3
+                if (newx>=0) and (newx<=18) and (newy>=0) and (newy<=18):
+                    if state[0][newx][newy]+state[1][newx][newy]==0:
+                        k=checkstate(newx,newy)
+                        if (k==3):
+                            return 3
+                        if (k==1) and (current in [0,2]):
+                            current=current+1
+                        if (k==2) and (current in [0,1]):
+                            current=current+2
+
+                    #0,
+                    if state[0][newx][newy]==1:
+                        # same as k==1
+                        if (current in [0,2]):
+                            current=current+1
+                    if state[1][newx][newy]==1:
+                        if (current in [0,1]):
+                            current=current+2
+                                
+        return current
+    w=0
+    lis=[0]
+    for i in range(19):
+        for j in range(19):
+            if issearched[i][j]==0:
+                if state[0][i][j]+state[1][i][j]==0:
+                    w=w+1
+                    now=checkstate(i,j,w)
+                    lis.append(now)
+    score=[0,0]
+    for i in range(19):
+        for j in range(19):
+            if state[0][i][j]==1:
+                score[0]+=1
+            if state[1][i][j]==1:
+                score[1]+=1
+            if state[0][i][j]+state[1][i][j]==0:
+                sc=lis[int(issearched[i][j])]
+                if sc==1:
+                    score[0]+=1
+                if sc==2:
+                    score[1]+=1
+
+    if state[16][0][0]==1:
+        return score[0]-score[1]-komi
+    else:
+        return score[0]-score[1]+komi
+
     return 0
 
 
